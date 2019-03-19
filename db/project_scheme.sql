5cce9c4297f4edb27d6fdaad48066672e52ddb11
Alena <alena.chuvasova@gmail.com>
2019-03-19 19:53:12 +0300

    change size of data types

--- db/project_scheme.sql
+++ db/project_scheme.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `project` (
   `id` int(11) NOT NULL,
   `name` varchar(45) NOT NULL,
-  `logoUrl` varchar(45) NOT NULL,
+  `logoUrl` varchar(200) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   `manHours` int(11) NOT NULL,
-  `code` varchar(45) NOT NULL,
-  `colour` varchar(45) NOT NULL,
-  `description` varchar(45) NOT NULL,
+  `code` varchar(6) NOT NULL,
+  `colour` varchar(6) NOT NULL,
+  `description` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
