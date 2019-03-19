60381a60b425ec7929f2eb1de0f971bc58451015
Ilia G <ilia119@tut.by>
2019-03-19 21:22:43 +0200

    Adding notification scheme

--- db/project_scheme.sql
+++ db/project_scheme.sql
@@ -42,3 +42,14 @@ CREATE TABLE `logs` (
   `date` date NOT NULL,
   PRIMARY KEY (`projectId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+create table Notification (
+id bigint NOT NULL PRIMARY KEY,
+createdAt DATETIME NOT NULL,
+employeId bigint NOT NULL,
+status varchar(100) NOT NULL,
+title varchar(100) NOT NULL,
+description text(200),
+link varchar(100)
+);
