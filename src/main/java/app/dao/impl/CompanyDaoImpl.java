cbf54ddb718ff04dfb1ca7f84ee5efb5efffd19f
TictacT0e <11sayros11@gmail.com>
2019-03-21 01:10:12 +0300

    jdbc example for company

--- src/main/java/app/dao/impl/CompanyDaoImpl.java
+++ src/main/java/app/dao/impl/CompanyDaoImpl.java
@@ -1,6 +1,7 @@
 package app.dao.impl;
 
 import app.dao.CompanyDao;
+import app.dao.database.DBConnection;
 import app.entities.Company;
 import app.entities.Employee;
 import app.entities.Project;
@@ -8,6 +9,10 @@ import app.exceptions.EntityAlreadyExistsException;
 import app.exceptions.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -23,35 +28,73 @@ public class CompanyDaoImpl implements CompanyDao {
 
     @Override
     public synchronized List<Company> getAll() {
-        return companies;
+        List<Company> _companies = null;
+        try {
+            Statement statement = DBConnection.getInstance()
+                    .getConnection().createStatement();
+            ResultSet resultSet = statement
+                    .executeQuery("SELECT * FROM timesheet_dev.Company");
+            _companies = getCompanies(resultSet);
+            statement.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return _companies;
     }
 
     @Override
     public synchronized Company findById(int id) {
-        for (Company company : companies) {
-            if (company.getId() == id) {
-                return company;
-            }
+        Company company = null;
+        try {
+            PreparedStatement preparedStatement =
+                    DBConnection.getInstance().getConnection()
+                            .prepareStatement("SELECT * FORM timesheet_dev.Company " +
+                                    "WHERE id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            company = mapping(resultSet);
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
         }
-        throw new EntityNotFoundException();
+        if(company == null) {
+            throw new EntityNotFoundException();
+        }
+        return company;
     }
 
     @Override
     public synchronized void save(Company company) {
-        for (Company savedCompany : companies) {
-            if (savedCompany.getId() == company.getId()) {
-                throw new EntityAlreadyExistsException();
-            }
+        try {
+            PreparedStatement preparedStatement =
+                    DBConnection.getInstance().getConnection()
+                            .prepareStatement("INSERT INTO timesheet_dev.Company " +
+                                    "(id, companyName, logoUrl) " +
+                                    "VALUE (?, ?, ?)");
+            preparedStatement.setInt(1, company.getId());
+            preparedStatement.setString(2, company.getName());
+            preparedStatement.setString(3, company.getLogoUrl());
+            preparedStatement.execute();
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
         }
-        companies.add(company);
     }
 
     @Override
     public synchronized void delete(Company retiringCompany) {
-        if (!companies.contains(retiringCompany)) {
-            throw new EntityNotFoundException();
+        try {
+            PreparedStatement preparedStatement =
+                    DBConnection.getInstance().getConnection()
+                            .prepareStatement("DELETE FROM timesheet_dev.Company " +
+                                    "WHERE id=?");
+            preparedStatement.setInt(1, retiringCompany.getId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
         }
-        companies.remove(retiringCompany);
+
     }
 
     @Override
@@ -61,10 +104,21 @@ public class CompanyDaoImpl implements CompanyDao {
 
     @Override
     public synchronized void edit(Company company) {
-        Company oldCompany = findById(company.getId());
-        int index = companies.indexOf(oldCompany);
-        companies.remove(oldCompany);
-        companies.add(index, company);
+        try {
+            PreparedStatement preparedStatement =
+                    DBConnection.getInstance().getConnection()
+                            .prepareStatement("UPDATE timesheet_dev.Company " +
+                                    "SET companyName=?, " +
+                                    "logoUrl=?, " +
+                                    "WHERE id=?");
+            preparedStatement.setString(1, company.getName());
+            preparedStatement.setString(2, company.getLogoUrl());
+            preparedStatement.setInt(3, company.getId());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
@@ -85,5 +139,25 @@ public class CompanyDaoImpl implements CompanyDao {
     @Override
     public synchronized List<Project> getCompanyProjects(Company company) {
         return company.getProjects();
+    }
+
+    private List<Company> getCompanies(ResultSet resultSet)
+            throws SQLException {
+        List<Company> _companies = new LinkedList<>();
+        Company company;
+        while (resultSet.next()) {
+            company = mapping(resultSet);
+            _companies.add(company);
+        }
+        return _companies;
+    }
+
+    private Company mapping(ResultSet resultSet)
+            throws SQLException {
+        Company company = new Company();
+        company.setId(resultSet.getInt("id"));
+        company.setName(resultSet.getString("companyName"));
+        company.setLogoUrl(resultSet.getString("logoUrl"));
+        return company;
     }
 }
