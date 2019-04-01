4afc1ae0f59a6cda9238477a5d6b83d6322e5fd9
TictacT0e <11sayros11@gmail.com>
2019-04-02 00:56:27 +0300

    All Assignment crud tests

--- src/test/java/app/dao/assignment/DeleteTests.java
+++ src/test/java/app/dao/assignment/DeleteTests.java
@@ -1,5 +1,6 @@
 package app.dao.assignment;
 
+import app.entities.Assignment;
 import app.exceptions.EntityNotFoundException;
 import org.dbunit.Assertion;
 import org.dbunit.dataset.IDataSet;
@@ -10,8 +11,21 @@ import org.junit.Test;
 public class DeleteTests extends AssignmentDaoTestsInitiator {
 
     @Test
-    public void delete() throws Exception {
+    public void deleteIdInParameters() throws Exception {
         assignmentDao.delete(2, 1);
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder()
+                .build(getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("deleteDataSets/delete-dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable(assignmentTable);
+        IDataSet actualDataSet = getMySqlConnection().createDataSet();
+        ITable actualTable = actualDataSet.getTable(assignmentTable);
+        Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void deleteObjectInParameters() throws Exception {
+        assignmentDao.delete(new Assignment(2, 1, 8000));
         IDataSet expectedDataSet = new FlatXmlDataSetBuilder()
                 .build(getClass()
                         .getClassLoader()
---
+++ src/test/java/app/dao/assignment/EditTests.java
@@ -0,0 +1,31 @@
+package app.dao.assignment;
+
+import app.entities.Assignment;
+import app.exceptions.EntityNotFoundException;
+import org.dbunit.Assertion;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+public class EditTests extends AssignmentDaoTestsInitiator {
+
+    @Test
+    public void edit() throws Exception {
+        Assignment assignment = new Assignment(1, 2, 15000);
+        assignmentDao.edit(assignment);
+        IDataSet expectedDataSet = new FlatXmlDataSetBuilder()
+                .build(getClass()
+                        .getClassLoader()
+                        .getResourceAsStream("editDataSets/edit-dataset.xml"));
+        ITable expectedTable = expectedDataSet.getTable(assignmentTable);
+        IDataSet actualDataSet = getMySqlConnection().createDataSet();
+        ITable actualTable = actualDataSet.getTable(assignmentTable);
+        Assertion.assertEquals(expectedTable, actualTable);
+    }
+
+    @Test(expected = EntityNotFoundException.class)
+    public void editWithNonExistsPrimaryKey() {
+        assignmentDao.edit(new Assignment(4, 7, 15000));
+    }
+}
