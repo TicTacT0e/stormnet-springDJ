c145efe086a4a15fd187bc472e13d98326976a98
Kiryl_Shalpuk <kirill.shelpuk@gmail.com>
2019-04-02 14:31:19 +0300

    updated SaveInvitationDaoTest

--- src/test/java/app/dao/Invitation/SaveInvitationDaoTest.java
+++ src/test/java/app/dao/Invitation/SaveInvitationDaoTest.java
@@ -1,4 +1,39 @@
 package app.dao.Invitation;
 
-public class SaveInvitationDaoTest {
+import app.entities.Invitation;
+import org.dbunit.Assertion;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Test;
+
+import java.sql.Date;
+import java.sql.SQLException;
+
+public class SaveInvitationDaoTest extends InitilizationInvitationDaoTest {
+
+    @Test
+    public void saveInvitation() {
+        invitationDao.save(new Invitation(
+                1, 1, "email",
+                "invatationCode", new Date(2019 - 03 - 03), "status"
+        ));
+        try {
+            IDataSet iDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                    .getResourceAsStream("/datasets/Invitation/save-dataset.xml"));
+            ITable iTable = iDataSet.getTable(table);
+
+            IDataSet actualDataSet = getMySqlConnection().createDataSet();
+            ITable actualITAble = actualDataSet.getTable(table);
+
+            Assertion.assertEquals(iTable, actualITAble);
+        } catch (DataSetException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
