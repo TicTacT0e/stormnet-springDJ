f201eadf5cc787773cf62873501c70305882c6bd
Kiryl_Shalpuk <kirill.shelpuk@gmail.com>
2019-04-02 14:09:53 +0300

    updated GetAllInvitationDaoTest

--- src/test/java/app/dao/Invitation/DeleteInvitationDaoTest.java
+++ src/test/java/app/dao/Invitation/DeleteInvitationDaoTest.java
@@ -5,24 +5,23 @@ import org.dbunit.Assertion;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.ITable;
-import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.Test;
 
 import java.sql.Date;
 import java.sql.SQLException;
 
-public class DeleteInvitationDaoTest extends InitilizationInvitationDaoTest{
+public class DeleteInvitationDaoTest extends InitilizationInvitationDaoTest {
 
     @Test
-    public void deleteById(){
+    public void deleteById() {
         invitationDao.delete(1);
         try {
             IDataSet iDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
-                    .getResourceAsStream("datasets/Invitation/delete-dataset.xml"));
+                    .getResourceAsStream("/datasets/Invitation/delete-dataset.xml"));
             ITable iTable = iDataSet.getTable(table);
 
-            IDataSet actualDataSet= getMySqlConnection().createDataSet();
+            IDataSet actualDataSet = getMySqlConnection().createDataSet();
             ITable actualITable = actualDataSet.getTable(table);
             Assertion.assertEquals(iTable, actualITable);
         } catch (DataSetException e) {
@@ -35,10 +34,10 @@ public class DeleteInvitationDaoTest extends InitilizationInvitationDaoTest{
     }
 
     @Test
-    public void deleteByObject(){
+    public void deleteByObject() {
         invitationDao.delete(new Invitation(
                 1, 1, "email",
-                "invatationCode", new Date(2019-03-03), "status"
+                "invatationCode", new Date(2019 - 03 - 03), "status"
         ));
 
         try {
--- src/test/java/app/dao/Invitation/GetAllInvitationDaoTest.java
+++ src/test/java/app/dao/Invitation/GetAllInvitationDaoTest.java
@@ -1,4 +1,44 @@
 package app.dao.Invitation;
 
-public class GetAllInvitationDaoTest {
+import app.entities.Invitation;
+import org.dbunit.dataset.DataSetException;
+import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class GetAllInvitationDaoTest extends InitilizationInvitationDaoTest {
+
+    private static final int NUMBER_OF_FIRST_ROW = 0;
+
+    @Test
+    public void getAllInvitation() {
+        try {
+            IDataSet iDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader().
+                    getResourceAsStream("/datasets/Invitation/getAll-dataset.xml"));
+            ITable iTable = iDataSet.getTable(table);
+            List<Invitation> invitationList = invitationDao.getAll();
+            int index = NUMBER_OF_FIRST_ROW;
+            for (Invitation invitation : invitationList) {
+                Assert.assertEquals(iTable.getValue(index, "employeeId").toString(),
+                        String.valueOf(invitation.getEmployeeId()));
+                Assert.assertEquals(iTable.getValue(index, "companyId").toString(),
+                        String.valueOf(invitation.getCompanyId()));
+                Assert.assertEquals(iTable.getValue(index, "email").toString(),
+                        String.valueOf(invitation.getEmail()));
+                Assert.assertEquals(iTable.getValue(index, "invitationsCode").toString(),
+                        String.valueOf(invitation.getInvitationsCode()));
+                Assert.assertEquals(iTable.getValue(index, "dateEnd").toString(),
+                        String.valueOf(invitation.getDateEnd()));
+                Assert.assertEquals(iTable.getValue(index, "status").toString(),
+                        String.valueOf(invitation.getStatus()));
+                index++;
+            }
+        } catch (DataSetException e) {
+            e.printStackTrace();
+        }
+    }
 }
