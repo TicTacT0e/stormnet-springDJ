3096cc76bff10e24c8a38359f8e8d14529959644
igorkraskovskiii <46607868+igorkraskovskiii@users.noreply.github.com>
2019-04-16 21:25:09 +0300

    Update DaoConfig.java
    solve conflict

--- src/main/java/app/config/beans/DaoConfig.java
+++ src/main/java/app/config/beans/DaoConfig.java
@@ -6,13 +6,10 @@ import app.dao.CompanyDao;
 import app.dao.EmployeeDao;
 import app.dao.LogsDao;
 import app.dao.ProjectDao;
-<<<<<<< HEAD
-=======
 import app.dao.impl.CompanyDaoImpl;
 import app.dao.impl.EmployeeDaoImpl;
 import app.dao.impl.ProjectDaoImpl;
 import app.dao.*;
->>>>>>> 6a488d06e4074e7545d2b611da8dce9f58266dc6
 import app.dao.impl.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,12 +38,10 @@ public class DaoConfig {
     }
 
     @Bean
-<<<<<<< HEAD
     public AssignmentDao getAssignmentDao() {
         return new AssignmentDaoImpl();
     }
 
-=======
     public InvitationDao getInvitationDao(){
         return new InvitationDaoImpl();
     }
@@ -55,5 +50,4 @@ public class DaoConfig {
     public AssignmentDao getAssignmentDao() {
         return new AssignmentDaoImpl();
     }
->>>>>>> 6a488d06e4074e7545d2b611da8dce9f58266dc6
 }
