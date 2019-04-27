60b325724c7156c7d6550c3513a30cd45d6468e4
TictacT0e <11sayros11@gmail.com>
2019-04-27 18:18:57 +0300

    post method in setting resource

--- src/main/java/app/resources/SettingsResource.java
+++ src/main/java/app/resources/SettingsResource.java
@@ -1,13 +1,17 @@
 package app.resources;
 
 import app.dao.BasicCrudDao;
+import app.dto.CompanySettingsDto;
 import app.entities.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Component
 public class SettingsResource {
@@ -16,5 +20,13 @@ public class SettingsResource {
     BasicCrudDao<Settings> settingsDao;
 
     @POST
+    @Path("/company/{companyId}/settings")
     @Consumes(MediaType.APPLICATION_JSON)
+    public Response add(
+            @PathParam("companyId") int companyId,
+            CompanySettingsDto companySettingsDto
+    ) {
+
+        return Response.status(Response.Status.CREATED.getStatusCode()).build();
+    }
 }
