fc3568513ee7535ae3b9b7345544961c065c4b34
Kiryl_Shalpuk <kirill.shelpuk@gmail.com>
2019-05-03 08:43:27 +0300

    added mifort's db in pom.xml, deleted HibernateSessionFactoryUtil.java

--- src/main/java/app/dao/impl/InvitationDaoImpl.java
+++ src/main/java/app/dao/impl/InvitationDaoImpl.java
@@ -1,11 +1,11 @@
 package app.dao.impl;
 
-import app.config.beans.HibernateSessionFactoryUtil;
 import app.dao.BasicCrudDao;
 import app.entities.Invitation;
 import app.exceptions.EntityNotFoundException;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,10 +14,13 @@ import java.util.List;
 @Repository
 @Transactional
 public class InvitationDaoImpl implements BasicCrudDao<Invitation> {
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public Invitation findById(int id) {
-        Invitation invitation = HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Invitation.class, id);
+        Invitation invitation = sessionFactory.getCurrentSession()
+                .get(Invitation.class, id);
         if (invitation == null) {
             throw new EntityNotFoundException();
         }
@@ -26,44 +29,33 @@ public class InvitationDaoImpl implements BasicCrudDao<Invitation> {
 
     @Override
     public List<Invitation> findAll() {
-        List<Invitation> invitationList = (List<Invitation>) HibernateSessionFactoryUtil.getSessionFactory()
-                .openSession().createQuery("from Invitation").list();
-        return invitationList;
-    }
-
-    @Override
-    public void create(Invitation entity) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.save(entity);
-        transaction.commit();
-        session.close();
+        Query query
+                = sessionFactory.getCurrentSession()
+                .createQuery("from Invitation");
+        return query.getResultList();
     }
 
     @Override
     public void deleteById(int id) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(findById(id));
-        transaction.commit();
-        session.close();
+        sessionFactory.getCurrentSession()
+                .delete(findById(id));
+    }
+
+    @Override
+    public void create(Invitation entity) {
+        sessionFactory.getCurrentSession()
+                .save(entity);
     }
 
     @Override
     public void delete(Invitation entity) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.delete(entity);
-        transaction.commit();
-        session.close();
+        sessionFactory.getCurrentSession()
+                .delete(entity);
     }
 
     @Override
     public void update(Invitation entity) {
-        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
-        Transaction transaction = session.beginTransaction();
-        session.update(entity);
-        transaction.commit();
-        session.close();
+        sessionFactory.getCurrentSession()
+                .update(entity);
     }
 }
