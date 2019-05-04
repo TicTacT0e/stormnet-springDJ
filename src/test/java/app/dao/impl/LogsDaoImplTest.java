3cebca9962cfa66900bfc9ff49f59b7468b104ac
Igor <IgorKraskovskiii@gmail.com>
2019-05-04 14:16:06 +0300

    removed comment

--- src/test/java/app/dao/impl/LogsDaoImplTest.java
+++ src/test/java/app/dao/impl/LogsDaoImplTest.java
@@ -5,7 +5,6 @@ import app.config.beans.HibernateConfig;
 import app.config.beans.PropertyConfig;
 import app.dao.BasicCrudDao;
 import app.entities.Log;
-import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,33 +15,21 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = {PropertyConfig.class, DaoConfig.class, HibernateConfig.class})
 public class LogsDaoImplTest {
 
-    DateTime customDateTimeFromString = new DateTime("2018-05-05T10:11:12.123");
-
     @Autowired
-    BasicCrudDao <Log> l;
+    BasicCrudDao<Log> l;
 
- /*   @Test
-    public void testCreate() {
-        Log logs = new Log(1,2.0,3,"srgsg",customDateTimeFromString);
-         l.create(logs);
-        //Log logs = new Log();
-        //logs.setTime(1);
-      //l.create(logs);
-      //  Assert.assertEquals(1,1);
-    }*/
+    @Test
+    public void testDelete() {
+        l.deleteById(1);
+    }
 
-@Test
-    public void testDelete () {
-    l.deleteById(1);
-}
-
-@Test
+    @Test
     public void testFindAll() {
-    System.out.println(l.findAll());
-}
+        System.out.println(l.findAll());
+    }
 
-@Test
-    public void testFindId(){
-    System.out.println(l.findById(2));
-}
+    @Test
+    public void testFindId() {
+        System.out.println(l.findById(2));
+    }
 }
