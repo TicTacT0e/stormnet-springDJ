e2d927412418fe95679efa9668e3745982f075d2
Kiryl_Shalpuk <kirill.shelpuk@gmail.com>
2019-05-11 19:19:55 +0300

    fixed the code

--- src/main/java/app/dao/impl/InvitationDaoImpl.java
+++ src/main/java/app/dao/impl/InvitationDaoImpl.java
@@ -7,12 +7,10 @@ import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
-@Transactional
 public class InvitationDaoImpl implements BasicCrudDao<Invitation> {
     @Autowired
     private SessionFactory sessionFactory;
