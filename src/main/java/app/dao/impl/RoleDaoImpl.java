fcdb73ededaa1ccb0ad109b9f403d8c1e557b601
Maksim <nastena.tkachenko2005@mail.ru>
2019-05-22 18:51:54 +0300

    merge with master

--- src/main/java/app/dao/impl/RoleDaoImpl.java
+++ src/main/java/app/dao/impl/RoleDaoImpl.java
@@ -1,48 +1,6 @@
 package app.dao.impl;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-import java.util.List;
+import app.entities.Role;
 
-@Repository
-@Transactional
-public class RoleDaoImpl implements RoleDao {
-
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Override
-    public List<Role> findAll() {
-        Query query3
-                = sessionFactory.getCurrentSession()
-                .createQuery("from app.entities.Role");
-        return query3.getResultList();
-    }
-
-    @Override
-    public List<Role> findByCode(String code) {
-        Query query4
-                = sessionFactory.getCurrentSession()
-                .createQuery("from Role where code = :paramName");
-        query4.setParameter("paramName", code);
-        return query4.getResultList();
-    }
-
-    @Override
-    public void deleteByCode(String code) {
-        Query query5
-                = sessionFactory.getCurrentSession()
-                .createQuery("delete Role where code = :paramName");
-        query5.setParameter("paramName", code);
-        int rows = query5.executeUpdate();
-    }
-
-    @Override
-    public void create(Role entity) {
-        sessionFactory.getCurrentSession()
-                .save(entity);
-    }
+public class RoleDaoImpl extends BasicCrudDaoImpl<Role>  {
 }
