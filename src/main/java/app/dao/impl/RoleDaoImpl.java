9b90f6b51f9c02c408b892edc77af48e959aa88b
Maksim <nastena.tkachenko2005@mail.ru>
2019-05-23 18:42:25 +0300

    merge with  master

--- src/main/java/app/dao/impl/RoleDaoImpl.java
+++ src/main/java/app/dao/impl/RoleDaoImpl.java
@@ -3,5 +3,5 @@ package app.dao.impl;
 import app.entities.Role;
 
 public class RoleDaoImpl extends BasicCrudDaoImpl<Role>  {
-    
+
 }
--- src/main/java/app/entities/Employee.java
+++ src/main/java/app/entities/Employee.java
@@ -16,8 +16,9 @@ public class Employee {
     @Id
     private int id;
     private String name;
-    private String photo;
+    private String phone;
     private String email;
+    private String photoUrl;
 
     @ElementCollection
     @CollectionTable(name = "Project")
@@ -26,18 +27,20 @@ public class Employee {
     public Employee() {
     }
 
-    public Employee(int id, String name, String photo, String email) {
+    public Employee(int id, String name, String phone, String email, String photoUrl) {
         this.id = id;
         this.name = name;
-        this.photo = photo;
+        this.phone = phone;
         this.email = email;
+        this.photoUrl = photoUrl;
     }
 
     public Employee(Employee employee) {
         this(employee.getId(),
                 employee.getName(),
-                employee.getPhoto(),
-                employee.getEmail());
+                employee.getPhone(),
+                employee.getEmail(),
+                employee.getPhotoUrl());
         this.projects = employee.getProjects();
     }
 
@@ -45,40 +48,48 @@ public class Employee {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getPhoto() {
-        return photo;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public List<Project> getProjects() {
-        return projects;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void setPhoto(String photo) {
-        this.photo = photo;
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
-    public void assignToProject(Project project) {
-        projects.add(project);
+    public String getPhotoUrl() {
+        return photoUrl;
+    }
+
+    public void setPhotoUrl(String photoUrl) {
+        this.photoUrl = photoUrl;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
     }
 
     @Override
@@ -92,14 +103,15 @@ public class Employee {
         Employee employee = (Employee) object;
         return id == employee.id
                 && Objects.equals(name, employee.name)
-                && Objects.equals(photo, employee.photo)
+                && Objects.equals(phone, employee.phone)
                 && Objects.equals(email, employee.email)
+                && Objects.equals(photoUrl, employee.photoUrl)
                 && Objects.equals(projects, employee.projects);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, photo, email, projects);
+        return Objects.hash(id, name, phone, email, photoUrl, projects);
     }
 
     @Override
@@ -107,8 +119,9 @@ public class Employee {
         return "Employee{"
                 + "id=" + id
                 + ", name='" + name + '\''
-                + ", photo='" + photo + '\''
+                + ", photo='" + phone + '\''
                 + ", email='" + email + '\''
+                + ", email='" + photoUrl + '\''
                 + '\n'
                 + ", projects=" + projects
                 + '}';
