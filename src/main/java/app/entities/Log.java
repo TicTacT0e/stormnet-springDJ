62c39a5053e441145733a9dca4a03649d150bfe6
Igor <IgorKraskovskiii@gmail.com>
2019-05-26 14:23:14 +0300

    added test for log

--- src/main/java/app/entities/Log.java
+++ src/main/java/app/entities/Log.java
@@ -1,75 +1,80 @@
 package app.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-import java.util.Date;
-import java.util.Objects;
+import java.sql.Timestamp;
+import java.util.StringJoiner;
 
 @Entity
-@Table(name = "logs")
+@Table(name = "Logs")
 public class Log {
 
     @Id
-    private int id;
-    private int assignmentId;
-    private double time;
-    private int order;
+    @Column(name = "id")
+    private Integer id;
+
+    private Integer assignmentId;
+    private Double time;
+    private Integer rowCount;
     private String comment;
-    private Date date;
+    private Timestamp date;
 
     public Log() {
     }
 
-    public Log(int id, int assignmentId, double time, int order,
-               String comment, Date date) {
+    public Log(int id, int assignmentId, double time, int rowCount, String comment) {
         this.id = id;
         this.assignmentId = assignmentId;
         this.time = time;
-        this.order = order;
+        this.rowCount = rowCount;
         this.comment = comment;
         this.date = date;
     }
 
-    public Log(int assignmentId, double time, int order,
-               String comment, Date date) {
-        this.assignmentId = assignmentId;
-        this.time = time;
-        this.order = order;
-        this.comment = comment;
-        this.date = date;
+    @PrePersist
+    protected void onCreate() {
+        date = new Timestamp(new java.util.Date().getTime());
     }
 
-    public int getId() {
+    @PreUpdate
+    protected void onUpdate() {
+        date = new Timestamp(new java.util.Date().getTime());
+    }
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public int getAssignmentId() {
+    public Integer getAssignmentId() {
         return assignmentId;
     }
 
-    public void setAssignmentId(int assignmentId) {
+    public void setAssignmentId(Integer assignmentId) {
         this.assignmentId = assignmentId;
     }
 
-    public double getTime() {
+    public Double getTime() {
         return time;
     }
 
-    public void setTime(double time) {
+    public void setTime(Double time) {
         this.time = time;
     }
 
-    public int getOrder() {
-        return order;
+    public Integer getRowCount() {
+        return rowCount;
     }
 
-    public void setOrder(int order) {
-        this.order = order;
+    public void setRowCount(Integer rowCount) {
+        this.rowCount = rowCount;
     }
 
     public String getComment() {
@@ -80,44 +85,23 @@ public class Log {
         this.comment = comment;
     }
 
-    public Date getDate() {
+    public Timestamp getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(Timestamp date) {
         this.date = date;
     }
 
     @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-        Log logs = (Log) object;
-        return id == logs.id
-                && assignmentId == logs.assignmentId
-                && Double.compare(logs.time, time) == 0
-                && order == logs.order
-                && Objects.equals(comment, logs.comment)
-                && Objects.equals(date, logs.date);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, assignmentId, time, order, comment, date);
-    }
-
-    @Override
     public String toString() {
-        return "Log{"
-                + "assignmentId: " + assignmentId + ';'
-                + "order: " + order + ';'
-                + " time: " + time + ';'
-                + "comment: '" + comment + '\''
-                + " Date: " + date + ';'
-                + '}';
+        return new StringJoiner(", ", Log.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("assignmentId=" + assignmentId)
+                .add("time=" + time)
+                .add("rowCount=" + rowCount)
+                .add("comment='" + comment + "'")
+                .add("date=" + date)
+                .toString();
     }
 }
--- src/test/java/app/dao/impl/LogsDaoImplTest.java
+++ src/test/java/app/dao/impl/LogsDaoImplTest.java
@@ -1,35 +1,87 @@
 package app.dao.impl;
 
-import app.config.beans.DaoConfig;
-import app.config.beans.HibernateConfig;
-import app.config.beans.PropertyConfig;
 import app.dao.BasicCrudDao;
 import app.entities.Log;
+import org.dbunit.Assertion;
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.dataset.ITable;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {PropertyConfig.class, DaoConfig.class, HibernateConfig.class})
-public class LogsDaoImplTest {
+import java.sql.SQLException;
+import java.util.List;
+
+public class LogsDaoImplTest extends ConnectionForTests {
 
     @Autowired
-    BasicCrudDao<Log> l;
+    private BasicCrudDao<Log> logDao;
 
-    @Test
-    public void testDelete() {
-        l.deleteById(1);
+    private String table = "Logs";
+    private String[] columnsToIgnore = {"date"};
+
+    public LogsDaoImplTest() {
+        super("app/dao/impl/logDataSet/input.xml"); //create input.xml.xml
     }
 
     @Test
-    public void testFindAll() {
-        System.out.println(l.findAll());
+    public void testFindAll() throws SQLException, DatabaseUnitException {
+        List<Log> logs = logDao.findAll();
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                .getResourceAsStream("app/dao/impl/logDataSet/input.xml")).getTable(table);
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        Assertion.assertEquals(expectedDataSet, actualDataSet);
+        Assert.assertEquals(expectedDataSet.getRowCount(), logs.size());
     }
 
     @Test
-    public void testFindId() {
-        System.out.println(l.findById(2));
+    public void testfindById() throws SQLException, DatabaseUnitException {
+        Log log = logDao.findById(1);
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                .getResourceAsStream("app/dao/impl/logDataSet/input.xml")).getTable(table);
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        String expectedTimeOfLog = (String) expectedDataSet.getValue(0, "comment");
+        Assertion.assertEquals(expectedDataSet, actualDataSet);
+        Assert.assertEquals(expectedTimeOfLog, log.getComment());
+    }
+
+    @Test
+    public void testCreate() throws SQLException, DatabaseUnitException {
+        //Timestamp date = new Timestamp(2019-05-04);
+        logDao.create(new Log(4, 4, 4, 4, "comment4"));
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                .getResourceAsStream("app/dao/impl/logDataSet/createExpected.xml")).getTable(table);
+        Assertion.assertEqualsIgnoreCols(expectedDataSet, actualDataSet, columnsToIgnore); //, columnsToIgnore
+    }
+
+    @Test
+    public void testDeleteById() throws SQLException, DatabaseUnitException {
+        logDao.deleteById(2);
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                .getResourceAsStream("app/dao/impl/logDataSet/deleteExpected.xml")).getTable(table);
+        Assertion.assertEquals(expectedDataSet, actualDataSet);
+    }
+
+    @Test
+    public void testDeleteEntity() throws SQLException, DatabaseUnitException {
+       // Timestamp date = new Timestamp(2019 - 05 - 12);
+        logDao.delete(new Log(2, 2, 2, 2, "comment2"));
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass()
+                .getClassLoader().getResourceAsStream("app/dao/impl/logDataSet/deleteExpected.xml")).getTable(table);
+        Assertion.assertEquals(expectedDataSet, actualDataSet);
+    }
+
+    @Test
+    public void testUpdate() throws SQLException, DatabaseUnitException {
+        // Timestamp date = new Timestamp(2019-05-10);
+        logDao.update(new Log(3, 10, 10, 10, "comment10"));
+        ITable actualDataSet = connection.createDataSet().getTable(table);
+        ITable expectedDataSet = new FlatXmlDataSetBuilder().build(getClass().getClassLoader()
+                .getResourceAsStream("app/dao/impl/logDataSet/updateExpected.xml")).getTable(table);
+        Assertion.assertEqualsIgnoreCols(expectedDataSet, actualDataSet, columnsToIgnore);//, columnsToIgnore
     }
 }
