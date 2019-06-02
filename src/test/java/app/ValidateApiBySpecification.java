04a66199b34eda9fbf3e796a88a2a8fa7deb30bd
TictacT0e <11sayros11@gmail.com>
2019-06-02 18:01:57 +0300

    added up all tests for assignment resource; use api-spec.json against .yaml

--- src/test/java/app/ValidateApiBySpecification.java
+++ src/test/java/app/ValidateApiBySpecification.java
@@ -8,8 +8,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static com.atlassian.oai.validator.mockmvc.OpenApiValidationMatchers.openApi;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 
 public class ValidateApiBySpecification {
@@ -17,6 +19,11 @@ public class ValidateApiBySpecification {
     private static final String SWAGGER_JSON_SPEC_PATH = "/swagger/api-spec.json";
 
     private MockMvc mockMvc;
+
+    private static final String ASSIGNMENT_TEST_JSON_MODEL = "{\"employeeName\" : \"name\", " +
+            "\"activities\" : [{" +
+            "\"name\" : \"name\"}], " +
+            "\"workLoad\" : 100}";
 
     @Before
     public void setup() {
@@ -26,8 +33,13 @@ public class ValidateApiBySpecification {
 
     @Test
     public void assignmentGetAll() throws Exception {
-        mockMvc.perform(get("company/1/assignment/1")
-                .header("api_key", "API_KEY"))
+        mockMvc.perform(get("company/1/assignment"))
+                .andExpect(openApi().isValid(SWAGGER_JSON_SPEC_PATH));
+    }
+
+    @Test
+    public void assignmentGet() throws Exception {
+        mockMvc.perform(get("company/1/assignment/1"))
                 .andExpect(openApi().isValid(SWAGGER_JSON_SPEC_PATH));
     }
 
@@ -36,10 +48,22 @@ public class ValidateApiBySpecification {
         mockMvc.perform(post("company/1/assignment")
                 .characterEncoding("UTF-8")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"employeeName\" : \"name\", " +
-                        "\"activities\" : [{" +
-                        "\"name\" : \"name\"}], " +
-                        "\"workLoad\" : 100}"))
+                .content(ASSIGNMENT_TEST_JSON_MODEL))
+                .andExpect(openApi().isValid(SWAGGER_JSON_SPEC_PATH));
+    }
+
+    @Test
+    public void assignmentPut() throws Exception {
+        mockMvc.perform(put("company/1/assignment/1")
+                .characterEncoding("UTF-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ASSIGNMENT_TEST_JSON_MODEL))
+                .andExpect(openApi().isValid(SWAGGER_JSON_SPEC_PATH));
+    }
+
+    @Test
+    public void assignmentDelete() throws Exception {
+        mockMvc.perform(delete("company/1/assignment/1"))
                 .andExpect(openApi().isValid(SWAGGER_JSON_SPEC_PATH));
     }
 }
