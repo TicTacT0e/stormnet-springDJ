3123526d26a54a312efb42352f7c251486fa8bcc
TictacT0e <11sayros11@gmail.com>
2019-06-23 10:46:50 +0300

    set up OAuth2CodeGrantFlow

--- src/main/java/app/resources/OAuth2Resource.java
+++ src/main/java/app/resources/OAuth2Resource.java
@@ -6,12 +6,14 @@ import org.glassfish.jersey.client.oauth2.ClientIdentifier;
 import org.glassfish.jersey.client.oauth2.OAuth2ClientSupport;
 import org.glassfish.jersey.client.oauth2.OAuth2CodeGrantFlow;
 import org.glassfish.jersey.client.oauth2.OAuth2FlowGoogleBuilder;
+import org.glassfish.jersey.jackson.JacksonFeature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
@@ -40,6 +42,12 @@ public class OAuth2Resource {
                 UriBuilder.fromUri(uriInfo.getBaseUri())
                 .path("oauth2/authorize").build().toString();
 
+        OAuth2CodeGrantFlow.Builder builder =
+                OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(
+
+                )
+
+
         final OAuth2CodeGrantFlow flow = OAuth2ClientSupport
                 .googleFlowBuilder(
                         OAuth2Service.getClientIdentifier(),
@@ -50,19 +58,29 @@ public class OAuth2Resource {
         OAuth2Service.setFlow(flow);
         final String googleAuthURI = flow.start();
 
+
         return Response.seeOther(UriBuilder.fromUri(googleAuthURI).build()).build();
     }
 
+//    @GET
+//    @Path("/authorize")
+//    public Response authorize()
+
+
     @GET
-    @Path("/wtf")
+    @Path("/secured")
     @Produces("text/html")
-    public Response setup() {
-        OAuth2Service.setClientIdentifier(new ClientIdentifier(
-                oAuth2PropertyProvider.getClientIdGoogle(),
-                oAuth2PropertyProvider.getClientSecretGoogle()
-        ));
-        final URI uri = UriBuilder.fromUri(uriInfo.getBaseUri())
-                .path("oauth2callback").build();
-        return Response.seeOther(uri).build();
+    public Response securedEndPoint() {
+        if (OAuth2Service.getAccessToken() == null) {
+            return googleAuthentication();
+        }
+
+        final Client client = OAuth2Service.getFlow().getAuthorizedClient();
+        return null;
     }
+
+//    private Response getSecuredInfo(final Client client) {
+//        client.register(JacksonFeature.class);
+//    }
+
 }
