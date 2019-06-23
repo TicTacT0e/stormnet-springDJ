9cc8408cc38586c9d6968a931b3c398b446beb58
TictacT0e <11sayros11@gmail.com>
2019-06-23 12:18:11 +0300

    set up OAuth2CodeGrantFlow finish in authorize method for getting access token

--- src/main/java/app/resources/OAuth2Resource.java
+++ src/main/java/app/resources/OAuth2Resource.java
@@ -6,22 +6,22 @@ import org.glassfish.jersey.client.oauth2.ClientIdentifier;
 import org.glassfish.jersey.client.oauth2.OAuth2ClientSupport;
 import org.glassfish.jersey.client.oauth2.OAuth2CodeGrantFlow;
 import org.glassfish.jersey.client.oauth2.OAuth2FlowGoogleBuilder;
-import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.client.oauth2.TokenResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
-import java.net.URI;
 
 @Component
-@Path("/login")
+@Path("/oauth2")
 public class OAuth2Resource {
 
     @Context
@@ -30,8 +30,18 @@ public class OAuth2Resource {
     @Autowired
     private OAuth2PropertyProvider oAuth2PropertyProvider;
 
+
+    private String clientSecret = "8UL-DCcDlQD1jLjh2tehN-42";
+    private String clientId = "741125894809-j3tls88pr4u1lmadblb85rv086ecnsvp.apps.googleusercontent.com";
+    private String userAuthirizationUri = "https://accounts.google.com/o/oauth2/v2/auth";
+    private String scope = "https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/userinfo.profile";
+    private String accessTokenUri = "https://www.googleapis.com/oauth2/v4/token";
+    private String tokenName = "authorization_code";
+    private String oauth2FilterCallbackPath = "/oauth2/callback";
+    private String userInfoUri = "https://www.googleapis.com/oauth2/v3/userinfo";
+
     @GET
-    @Path("/google")
+    @Path("/login/google")
     @Produces("text/html")
     public Response googleAuthentication() {
         OAuth2Service.setClientIdentifier(new ClientIdentifier(
@@ -40,12 +50,20 @@ public class OAuth2Resource {
         ));
         final String redirectURI =
                 UriBuilder.fromUri(uriInfo.getBaseUri())
-                .path("oauth2/authorize").build().toString();
+                        .path("/oauth2/authorize").build().toString();
 
-        OAuth2CodeGrantFlow.Builder builder =
-                OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(
+//        OAuth2CodeGrantFlow.Builder builder =
+//                OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(
+//                        OAuth2Service.getClientIdentifier(),
+//                        redirectURI,
+//                        accessTokenUri
+//                );
 
-                )
+//        OAuth2CodeGrantFlow flow = builder
+//                .property(OAuth2CodeGrantFlow.Phase.AUTHORIZATION,
+//                        "readOnly", "true")
+//                .scope(scope)
+//                .build();
 
 
         final OAuth2CodeGrantFlow flow = OAuth2ClientSupport
@@ -55,6 +73,7 @@ public class OAuth2Resource {
                         "https://www.googleapis.com/auth/plus.login"
                 )
                 .prompt(OAuth2FlowGoogleBuilder.Prompt.CONSENT).build();
+
         OAuth2Service.setFlow(flow);
         final String googleAuthURI = flow.start();
 
@@ -62,9 +81,18 @@ public class OAuth2Resource {
         return Response.seeOther(UriBuilder.fromUri(googleAuthURI).build()).build();
     }
 
-//    @GET
-//    @Path("/authorize")
-//    public Response authorize()
+    @GET
+    @Path("/authorize")
+    public String authorize(
+            @QueryParam("code") String code,
+            @QueryParam("state") String state
+    ) {
+        final OAuth2CodeGrantFlow flow = OAuth2Service.getFlow();
+        final TokenResult tokenResult = flow.finish(code, state);
+        OAuth2Service.setAccessToken(tokenResult.getAccessToken());
+//        return Response.ok("Got it!").build();
+        return OAuth2Service.getAccessToken();
+    }
 
 
     @GET
