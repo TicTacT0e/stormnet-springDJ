06e76e2aac43f7ad9e2c1e22a4b60c1a9b2b6468
TictacT0e <11sayros11@gmail.com>
2019-07-07 14:15:41 +0300

    removed gson and pdfbox dependencies, because they ar useless in this case

--- src/main/java/app/services/PdfService.java
+++ src/main/java/app/services/PdfService.java
@@ -2,31 +2,46 @@ package app.services;
 
 
 import app.entities.Role;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class PdfService {
 
-    public static void main(String[] args) {
+    private static final String PATH_TO_DIRECTORY = "src/main/resources/pdfDocuments/";
+
+    public static void main(String[] args) throws IOException {
 
         Document document = new Document();
         try {
-            PdfWriter.getInstance(document, new FileOutputStream("helloWorld.pdf"));
+            PdfWriter.getInstance(document,
+                    new FileOutputStream(PATH_TO_DIRECTORY + "helloWorld.pdf"));
 
             document.open();
 //            Chunk chunk = new Chunk("Hello world");
             Role role = new Role("21", "WEwe");
-            document.add(role);
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            String objectInJsonString = objectMapper.writeValueAsString(role);
+            System.out.println(objectInJsonString);
+            Font font = FontFactory.getFont(FontFactory.COURIER, 11, BaseColor.BLACK);
+            Chunk chunk = new Chunk(objectInJsonString, font);
+            document.add(chunk);
         } catch (DocumentException | FileNotFoundException e) {
             e.printStackTrace();
         } finally {
             document.close();
         }
+        
 
     }
 
