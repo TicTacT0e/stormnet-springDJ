e3775dd035ae2d68065302ba9d5fa5775c268267
TictacT0e <11sayros11@gmail.com>
2019-07-07 16:11:35 +0300

    added up an PdfService class for pdf generation from some object

--- src/main/java/app/services/PdfService.java
+++ src/main/java/app/services/PdfService.java
@@ -1,7 +1,7 @@
 package app.services;
 
 
-import app.entities.Role;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chunk;
@@ -10,39 +10,38 @@ import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.pdf.PdfWriter;
+import org.joda.time.DateTime;
+import org.springframework.stereotype.Service;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
+@Service
 public class PdfService {
 
     private static final String PATH_TO_DIRECTORY = "src/main/resources/pdfDocuments/";
 
-    public static void main(String[] args) throws IOException {
-
+    public String generatePdfAndGetPathToFile(Object object) {
         Document document = new Document();
+        String pathToFile = PATH_TO_DIRECTORY
+                + object.getClass().getSimpleName()
+                + "_" + DateTime.now().getMillis();
         try {
             PdfWriter.getInstance(document,
-                    new FileOutputStream(PATH_TO_DIRECTORY + "helloWorld.pdf"));
-
+                    new FileOutputStream(pathToFile));
             document.open();
-//            Chunk chunk = new Chunk("Hello world");
-            Role role = new Role("21", "WEwe");
             ObjectMapper objectMapper = new ObjectMapper();
-
-            String objectInJsonString = objectMapper.writeValueAsString(role);
-            System.out.println(objectInJsonString);
-            Font font = FontFactory.getFont(FontFactory.COURIER, 11, BaseColor.BLACK);
-            Chunk chunk = new Chunk(objectInJsonString, font);
+            String jsonObject = objectMapper.writeValueAsString(object);
+            Font font = FontFactory
+                    .getFont(FontFactory.COURIER, 11, BaseColor.BLACK);
+            Chunk chunk = new Chunk(jsonObject, font);
             document.add(chunk);
-        } catch (DocumentException | FileNotFoundException e) {
-            e.printStackTrace();
+        } catch (FileNotFoundException | JsonProcessingException |
+                DocumentException exception) {
+            exception.printStackTrace();
         } finally {
             document.close();
         }
-        
-
+        return pathToFile;
     }
-
 }
